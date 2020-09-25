8b299a6f2848e444bb4ac13a5d2e861198c2e458
Kim Co <kimcocrisostomo@gmail.com>
2020-09-24 23:40:15 -0700

    Array

--- L6/Homework/homework.js
+++ L6/Homework/homework.js
@@ -49,33 +49,62 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  let arr1 = ["Kim", "Crisostomo."];
+  let arr2 = ["I", "play"];
+  let arr3 = ["I", "am"];
+  let arr4 = ["Valorant", "as", "Sage", "main!"];
+  return words = arr3.concat(arr1,arr2,arr4).join(' ');
 }
+console.log(wordsToSentence('words'))
 
 function contains(arr, item) {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
+  if (arr.includes(item, 0)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+console.log(contains(['Air', 'Wind', 'Fire'], 'Earth'))
+
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let sum = 0
+    for (let i = 0; i < arguments.length; i++) {
+      sum = sum + arguments[i];
+    }
+  return sum;
 }
+
+console.log(addNumbers(1, 2, 3, 4 ,5))
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let sum = 0
+    for (let i = 0; i < arguments.length; i++) {
+      sum = sum + arguments[i];
+    }
+  return sum/arguments.length;
 }
+console.log(averageTestScore(1, 2, 3, 4 ,5))
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  return Math.max.apply(null, numbers);
 }
+console.log(largestNumber([1, 2, 3, 4, 5]))
+
 
 function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
 }
+
 // Do not modify code below this line.
 // --------------------------------
 
